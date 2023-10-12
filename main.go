47b93e8b0342fa628b8eb75e455e0cb9bc04d394
two-six <twopsix@gmail.com>
2023-10-12 20:33:14 +0200

    min max functions

--- main.go
+++ main.go
@@ -78,6 +78,8 @@ func (m *neko) Update() error {
 	}
 	// Prevents neko from being stuck on the side of the screen
 	// or randomly travelling to another monitor
+	m.x = max(0, min(m.x, monitorWidth))
+	m.y = max(0, min(m.y, monitorWidth))
 	if m.x < 0 {
 		m.x = 0
 	} else if m.x >= monitorWidth {
